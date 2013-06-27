dea8e92ae223951f155a2a21262633129034857c
leeper <thosjleeper@gmail.com>
2013-06-27 22:16:19 +0200

    Expanded mergeNA to allow for arbitrary number of input vectors

--- R/mergeNA.r
+++ R/mergeNA.r
@@ -1,34 +1,28 @@
-# FUNCTION TO MERGE TWO VARIABLES (e.g., TWO VERSIONS OF A SURVEY QUESTION)
-
-# Copyright (C) 2011  Thomas J. Leeper
-# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-
-# a and b are two vectors of equal length, which are to be combined into one new vector
-# this is useful when a survey involves two forms, with each form coded separately
-
-# NEED TO UPDATE TO AN INFINITE NUMBER OF VARIABLES
-
-mergeNA <- function(a,b) {
-	if(!length(a)==length(b))
-		stop("length(a) != length(b)")
-	x <- NA
-	for(i in 1:length(a)){
-		if(!is.na(a[i]) & is.na(b[i]))
-			x[i] <- a[i]
-		else if(is.na(a[i]) & !is.na(b[i]))
-			x[i] <- b[i]
-		else if(is.na(a[i]) & is.na(b[i]))
-			x[i] <- NA
-		else if(!is.na(a[i]) & !is.na(b[i])){
-			if(a[i]==b[i])
-				x[i] <- a[i]
-			else{
-				x[i] <- NA
-				cat("Respondent ",i,": ",a[i],", ",b[i],"\n",sep="")
-			}
-		}
-	}
-	return(x)
+mergeNA <- function(...) {
+	vars <- list(...)
+	lengths <- sapply(vars,FUN=length)
+	# check variable lengths
+	if(!identical(rep(lengths[1],length(vars)), lengths))
+		stop("Vectors specified in '...' have different lengths")
+	# test for NAs in each vector
+	a <- lapply(vars, function(x) is.na(x))
+	amat <- do.call(cbind,a)
+	# check for mutual missigness
+	mutual <- apply(!amat, 1, sum) > 1
+	if(any(mutual) & sum(mutual)>=10)
+		stop("Missingness is not mutually exclusive at 10 or more indices")
+	else if(any(mutual))
+		stop("Missingness is not mutually exclusive at indices ",paste(which(mutual),collapse=","))
+	# positions of non-NAs in each vector
+	notNA <- apply(!amat, 2, which)
+	if(!is.list(notNA))
+		notNA <- lapply(seq_len(ncol(notNA)), function(i) notNA[,i])
+	# pairs of NA positions and mergeable values
+	p <- mapply(function(val,pos) val[pos], vars, notNA, SIMPLIFY=FALSE)
+	pairs <- cbind(unlist(notNA),unlist(p))
+	# start with first variable
+	out <- vars[[1]]
+	# replace NAs, if applicable with values from `pairs`
+	out[pairs[,1]] <- pairs[,2]
+	return(out)
 }
